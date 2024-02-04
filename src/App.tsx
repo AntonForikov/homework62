2fe0d0c829ced9ee5747da17daaabc9143684754
Anton Forikov <tohabass@gmail.com>
2024-02-04 19:10:19 +0600

    Added routes for new elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import AppBar from './components/AppBar/AppBar';
+import {Route, Routes} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import AboutUs from './containers/AboutUs/AboutUs';
+import Contacts from './containers/Contacts/Contacts';
 
 function App() {
 
   return (
     <>
-      <AppBar />
+      <header>
+        <AppBar />
+      </header>
+      <main className='container-fluid'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='about-us' element={<AboutUs />} />
+          <Route path='contacts' element={<Contacts />} />
+          <Route path='*' element={<h1>Not found!</h1>}/>
+        </Routes>
+      </main>
     </>
   );
 }
--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 const AppBar: React.FC = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link" href="#">Home</a>
-            <a className="nav-link" href="#">Features</a>
-            <a className="nav-link " href="#">Pricing</a>
-          </div>
-        </div>
+        <NavLink className="navbar-brand" to="#">Navbar</NavLink>
+
+        <ul className='navbar-nav ms-auto'>
+          <li>
+            <NavLink className="nav-link" to='/'>Home</NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link" to="/about-us">About us</NavLink>
+          </li>
+          <li>
+            <NavLink className="nav-link " to="/contacts">Contacts</NavLink>
+          </li>
+        </ul>
       </div>
     </nav>
   );
